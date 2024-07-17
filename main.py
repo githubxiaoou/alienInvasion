0f1d3fbf1349fe83681fef5d9957f4c11330f811
sunwenou <sunwenou@lrhealth.com>
2024-07-17 18:22:17 +0800

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+import sys
+
+import pygame
+
+from setting import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+
+def run_game():
+    # 初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Invasion')
+
+    # 创建Play按键
+    play_button = Button(ai_settings, screen, 'Play')
+
+    # 创建一个用于存储游戏统计信息的实例并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建一群外星人
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 开始游戏的主循环
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets, stats, sb)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+
+run_game()
